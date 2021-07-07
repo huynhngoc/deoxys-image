55bbe86ba9b9f911d06bede5ab925861a38a4b3b
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2021-07-07 10:15:25 +0200

    Verbal print in utils

--- src/deoxys_image/utils.py
+++ src/deoxys_image/utils.py
@@ -14,6 +14,7 @@ def get_num_cpus():
                 if not ray.is_initialized():
                     ray.init(include_dashboard=False,
                              _temp_dir=os.environ.get('RAY_ROOT'))
+                    print(f'Initialized ray on {NUM_CPUS} cpus')
         except Exception as e:
             NUM_CPUS = 1
             print(e, 'Running augmentation on single processors')
